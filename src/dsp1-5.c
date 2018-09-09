ff000724649a07c0fbbcad1d623efff30ab6eb6f
yuuki <user@yuuki-no-MacbookPro.local>
2018-09-10 08:59:33 +0900

    update all

--- src/dsp1-5.c
+++ src/dsp1-5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 #define NUM 500
 
@@ -8,7 +9,8 @@ typedef struct{
 	double im;
 }comp;
 
-void readData(comp *data, char *filename, int N);
+void inputData(comp *data, char *filename, int N);
+void outputData(comp *data, char *filename, int N);
 void dft(comp *xn, int N, comp *Xk, int a, int b);
 void ampSpectrum(comp *Xk, int N, double *spec);
 void phaSpectrum(comp *Xk, int N, double *spec);
@@ -21,8 +23,9 @@ int main(){
 	
 	int i = 0;
 	int N = NUM;
-	double buf[NUM]={0};
+	double buf[NUM]={0}, pha_buf[NUM]={0};
 	comp a[NUM];
+	FILE *fp;
 
 	for(i=0;i<N;i++){
 		Xk[i].re = 0;
@@ -33,35 +36,93 @@ int main(){
 		a[i].im = 0;
 	}
 
-	readData(xn, filename, N);
+	inputData(xn, filename, N);
 	dft(xn, N, Xk, 1, 1);
 	ampSpectrum(Xk, N, buf);
+	phaSpectrum(Xk, N, pha_buf);
 	dft(Xk, N, a, -1, N);
 
-	for(i=0;i<N;i++){
-		printf("xn[%d].re\t=%12lf\n", i, xn[i].re);
-		printf("xn[%d].im\t=%12lf\n", i, xn[i].im);
-		printf("a[%d].re\t=%12lf\n", i, a[i].re);
-		printf("a[%d].im\t=%12lf\n", i, a[i].im);
-		printf("Xk[%d].re\t=%12lf\n",i ,Xk[i].re);
-		printf("Xk[%d].im\t=%12lf\n",i ,Xk[i].im);
-		printf("amp[%d]=%12lf\n", i, buf[i]);
-	}	
+//	for(i=0;i<N;i++){
+//		printf("xn[%d].re\t=%12lf\n", i, xn[i].re);
+//		printf("xn[%d].im\t=%12lf\n", i, xn[i].im);
+//		printf("a[%d].re\t=%12lf\n", i, a[i].re);
+//		printf("a[%d].im\t=%12lf\n", i, a[i].im);
+//		printf("Xk[%d].re\t=%12lf\n",i ,Xk[i].re);
+//		printf("Xk[%d].im\t=%12lf\n",i ,Xk[i].im);
+//		printf("amp[%d]=%12lf\n", i, buf[i]);
+//	}
+
+	outputData(xn, "xn", N);
+	outputData(a, "a", N);
+	outputData(Xk, "Xk", N);
+	
+	fp = fopen("amp.txt", "w");
+
+	for(i = 0; i < N; i++){
+		if(fprintf(fp, "%lf\n", buf[i]) < 0 ){
+			printf("ERROR\n");
+			return 0;
+		}
+	}
+
+	fp = fopen("pha.txt", "w");
+
+	for(i = 0; i < N; i++){
+		if(fprintf(fp, "%lf\n", pha_buf[i]) < 0 ){
+			printf("ERROR\n");
+			return 0;
+		}
+	}
 
 	return 0;
 }
 
-void readData(comp *data, char *filename, int N){
+void inputData(comp *data, char *filename, int N){
 	FILE *fp;
 	int i=0;
 
 	fp = fopen(filename, "r");
 	for(i=0;i<N;i++){
 		if(fscanf(fp, "%lf\n", &data[i].re)==EOF){
-			printf("[ERROR] cant read file to the last.");
+			printf("[ERROR] cant read file to the end.");
 			return;
 		}
 	}
+}
+
+void outputData(comp *data, char *filename, int N){
+	FILE *fp_re, *fp_im;
+	int i = 0;
+	char filename_re[256];
+	char filename_im[256];
+
+	strcpy(filename_re, filename);
+	strcpy(filename_im, filename);
+	strcat(filename_re, "_re.csv");
+	strcat(filename_im, "_im.csv");
+
+	fp_re = fopen(filename_re, "w");
+	fp_im = fopen(filename_im, "w");
+	if(fp_re == NULL){
+		printf("cant open file.\n");
+	}
+	if(fp_im == NULL){
+		printf("cant open file.\n");
+	}
+
+	for(i = 0; i < N; i++){
+		if(fprintf(fp_re, "%lf,\n", data[i].re) < 0 ){
+			printf("ERROR\n");
+			return;
+		}
+		if(fprintf(fp_im, "%lf,\n", data[i].im) < 0){
+			printf("[ERROR] cant write file to the end.");
+			return;
+		}
+	}
+
+	fclose(fp_re);
+	fclose(fp_im);
 }
 
 void dft(comp *xn, int N, comp *Xk, int a, int b){
@@ -87,5 +148,6 @@ void phaSpectrum(comp *Xk, int N, double *spec){
 	int k=0;
 	for(k=0;k<N;k++){
 		spec[k] = atan(Xk[k].im/Xk[k].re);
+	
 	}
 }
